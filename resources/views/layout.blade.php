bf391eec43722fe6220fbde35efb2d2cf60373e4
Danny Mexen <dannymexen@gmail.com>
2019-01-29 19:17:25 +0200

    Added icons to the login

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,15 +4,23 @@
 <head>
     <title>{{ 'title', 'AppSysFVR' }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
+    <nav>
+        <div class="nav-wrapper teal lighten">
+            <a href="#" class="brand-logo center">Approval System for Vehicle Requisitions.</a>
+        </div>
+    </nav>
 
 
     <div class="container">
         @yield('content')
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,24 +1,31 @@
 @extends('layout') 
 @section('content')
 <div class="container">
-    <h3>Welcome. Please Login.</h3>
+    <div class="row">
+        <div class="card">
+
+            <form>
+                <div class="center">
+                    <h4 class="teal lighten white-text">Login</h4>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">face</i>
+                            <input type="text" id="username">
+                            <label for="username">Username</label>
+                        </div>
+                        <div class="input-field">
+                            <i class="material-icons prefix">https</i>
+                            <input type="password" id="password">
+                            <label for="password">Password</label>
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
 </div>
-
-<div class="container">
-    <form>
-
-        <div class="field">
-            <div>
-                <input type="text" placeholder="username">
-            </div>
-        </div>
-
-
-        <div class="field">
-            <div>
-                <input type="password" placeholder="password">
-            </div>
-        </div>
-    </form>
 </div>
 @endsection
