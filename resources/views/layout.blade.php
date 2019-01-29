d24ad6d9c2648a20297bb5211ec5f905f99e3e81
Danny Mexen <dannymexen@gmail.com>
2019-01-29 23:03:36 +0200

    responsive image

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,32 +3,52 @@
 
 <head>
     <title>{{ 'title', 'AppSys' }}</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <style type="text/css">
-        body {
-            background-image: url("/img/bgi.png");
-            background-repeat: no-repeat;
-            background-size: cover;
-        } 
+        @media only screen and (min-device-width: 700px) {
+
+            body {
+                background-image: url("/img/bgi2.png");
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-attachment: fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+        }
+        @media only screen and (min-device-width 260px) {
+            body {
+                background-color:antiquewhite;
+            }
+        }
     </style>
 </head>
 
 <body>
-    <nav>
-        <div class="nav-wrapper teal">
-            <a href="#" class="brand-logo center">AppSys - Vehicle Requisition Approval System</a>
+    <div class="row">
+        <nav>
+            <div class="nav-wrapper teal">
+                <a href="#" class="brand-logo center">AppSys<i class="large material-icons">drive_eta</i></a>
+            </div>
+        </nav>
+
+
+        <div class="container">
+            <div class="row">
+                @yield('content')
+            </div>
         </div>
-    </nav>
 
-
-    <div class="container">
-        @yield('content')
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,29 +5,31 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="card">
+        <div class="">
 
-            <form method="POST" action="/requisition">
+            <form method="POST" action="/requisition" class="col s12">
                 {{ csrf_field() }}
-                
+
                 <div class="center">
-                    <h4 class="teal lighten white-text">Login</h4>
+                    <h4 class="card-panel">Login To Requisition A Vehicle</h4>
                 </div>
                 <div class="container">
-                    <div class="row">
-                        <div class="input-field">
-                            <i class="material-icons prefix">face</i>
-                            <input type="text" id="username" name="username">
-                            <label for="username">Username</label>
-                        </div>
-                        <div class="input-field">
-                            <i class="material-icons prefix">lock</i>
-                            <input type="password" id="password">
-                            <label for="password">Password</label>
-                        </div>
-                        <div class="input-field">
-                            <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                    <div class="center card-panel">
+                        <div class="row">
+                            <div class="input-field">
+                                <i class="material-icons prefix">face</i>
+                                <input type="text" id="username" name="username">
+                                <label for="username">Username</label>
+                            </div>
+                            <div class="input-field">
+                                <i class="material-icons prefix">lock</i>
+                                <input type="password" id="password">
+                                <label for="password">Password</label>
+                            </div>
+                            <div class="input-field">
+                                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
                                         <i class="material-icons right">send</i></button>
+                            </div>
                         </div>
                     </div>
                 </div>
