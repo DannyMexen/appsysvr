b08aaeef348cbb14e447971bb09af061069ef90a
Danny Mexen <dannymexen@gmail.com>
2019-01-31 11:56:23 +0200

    icons on the right (consistency)

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="row navbar-fixed">
     <nav>
         <div class="nav-wrapper teal">
-            <a href="#" class="brand-logo left">John Banda<i class="large material-icons">person_pin</i></a>
-            <a href="#" class="brand-logo center">AppSys<i class="large material-icons">drive_eta</i></a>
+            <a href="#" class="brand-logo left">John Banda<i class="large material-icons right">person_pin</i></a>
+            <a href="#" class="brand-logo center">AppSys<i class="large material-icons right">drive_eta right</i></a>
             <ul class="right hide-on-med-and-down">
                 <li> <a class="waves-effect waves-light btn red">LOGOUT<i class="material-icons right">not_interested</i></a></li>
             </ul>
--- resources/views/navbar_login.blade.php
+++ resources/views/navbar_login.blade.php
@@ -2,7 +2,7 @@
 <div class="row navbar-fixed">
     <nav>
         <div class="nav-wrapper teal">
-            <a href="#" class="brand-logo center">AppSys<i class="large material-icons">drive_eta</i></a>
+            <a href="#" class="brand-logo center">AppSys<i class="large material-icons right">drive_eta</i></a>
         </div>
     </nav>
     <!-- padding -->
